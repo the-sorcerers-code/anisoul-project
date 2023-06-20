ff3435a97d1b69b9e731ecc386bcb5bd07a024b4
Kudzai <kudzaitsapo@gmail.com>
2023-06-20 13:42:42 +0200

    Added backend

---
+++ anisoul/src/main/java/com/sorcererscode/anisoul/AnisoulApplication.java
@@ -0,0 +1,13 @@
+package com.sorcererscode.anisoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnisoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnisoulApplication.class, args);
+	}
+
+}
---
+++ anisoul/src/test/java/com/sorcererscode/anisoul/AnisoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorcererscode.anisoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnisoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
